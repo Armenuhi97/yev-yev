00a78f12c2be040b4ad2fdbf9bff1136f7f2fa7d
Armenuhi1997 <armenuhimanukyan@annaniks.com>
2021-02-16 10:30:45 +0400

    login

--- src/app/com/annaniks/yev-yev/pages/auth/auth.component.ts
+++ src/app/com/annaniks/yev-yev/pages/auth/auth.component.ts
@@ -1,8 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-auth',
     templateUrl: 'auth.component.html',
     styleUrls: ['auth.component.scss']
 })
-export class AuthComponent { }
+export class AuthComponent implements OnInit, OnDestroy {
+    public loginForm: FormGroup;
+    constructor(private _fb: FormBuilder) { }
+
+    ngOnInit() {
+        this._initForm()
+    }
+    private _initForm() {
+        this.loginForm = this._fb.group({
+            email: [null, Validators.required],
+            password: [null, Validators.required]
+        })
+    }
+    public submitForm() { }
+    ngOnDestroy() { }
+}
